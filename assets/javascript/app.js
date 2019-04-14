6e0f0bd2eeb825056d0ac453936620b703054e22
MeenaKrs <46361811+MeenaKrs@users.noreply.github.com>
2019-04-13 22:41:07 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,34 @@
+// Global variables
 var questionTracker = -1, totalQuestions = 0, timeLeft = 16;
 var ansOptions = [], questions = [];
 var unanswered = 0, correctAnswers = 0, wrongAnswers = 0;
 
+// Constructor to build questions retrieved from API
 function frameQuestions(ques, ansCh, correct) {
-    console.log("ques: " + ques + "ans: " + ansCh + "correct: " + correct);
     this.q = ques;
     this.a = ansCh;
     this.c = correct;
-    console.log(this);
     questions.push(this);
 }
 
-function loading() {
-    $(".qBox").html("<h1>Loading...</h1>");
-}
-
+// Fetch questions from API
 function fetchQuestions() {
     $.ajax({
         url: "https://opentdb.com/api.php?amount=10&category=19&difficulty=medium",
         method: "GET"
     }).then(function (response) {
-        console.log(response);
         totalQuestions = response.results.length;
         for (let i = 0; i < response.results.length; i++) {
             let ques = response.results[i].question;
             ansCh = response.results[i].incorrect_answers;
             correct = response.results[i].correct_answer
             questions[i] = new frameQuestions(ques, ansCh, correct);
-            console.log(questions[i]);
         }
         nextQuestion();
     });
 }
 
+// Display next question
 function nextQuestion() {
     questionTracker++;
     ansOptions = [];
@@ -45,11 +41,11 @@ function nextQuestion() {
     for (let i = 0; i < questions[questionTracker].a.length; i++) {
         ansOptions.push(questions[questionTracker].a[i]);
     }
-    console.log("array: " + ansOptions);
+
+    // Shuffle options list
     ansOptions.sort(function () {
         return 0.5 - Math.random();
     });
-    console.log("array: " + ansOptions);
     for (let i = 0; i < ansOptions.length; i++) {
         var newBtn = $("<button>");
         newBtn.attr("class", "btn btn-lg m-2");
@@ -60,50 +56,58 @@ function nextQuestion() {
     timer = setInterval(startTimer, 1000);
 }
 
+// Timer
 function startTimer() {
     if (timeLeft > 0) {
         timeLeft--;
     } else {
         clearInterval(timer);
-        console.log("Missed:(");
         evaluateAnswer("missed");
         if (totalQuestions - (questionTracker + 1)) {
             setTimeout(nextQuestion, 3000);
         } else {
-            endGame();
+            setTimeout(endGame, 3000);
         }
     }
     $(".timer").text("Time Left: " + timeLeft);
 }
 
+// Answer evaluation
 function evaluateAnswer(status) {
 
     var gif = $("<img>");
+
+    // If user answer is correct
     if (status == "correct") {
         $(gif).attr("src", "assets/images/yougotit.gif");
         $(".choice").empty();
         $("#question").html("<p>You got it!!!</p>");
         correctAnswers++;
-        $(".correctAns").text(correctAnswers + "/10");
+        $(".correctAns").text(correctAnswers + "/" + totalQuestions);
+
+        // If user answer is wrong
     } else if (status == "wrong") {
         $(gif).attr("src", "assets/images/notthat.gif");
         $(".choice").html("<p> Correct answer is " + questions[questionTracker].c);
         wrongAnswers++;
-        $(".wrongAns").text(wrongAnswers + "/10");
+        $(".wrongAns").text(wrongAnswers + "/" + totalQuestions);
+
+        // If user missed to answer on right time
     } else if (status == "missed") {
         $(gif).attr("src", "assets/images/ohnoo.gif");
         $(".choice").html("<p>Time's up!!</p><p>Correct answer is " + questions[questionTracker].c + "</p>");
         unanswered++;
-        $(".unAns").text(unanswered + "/10");
+        $(".unAns").text(unanswered + "/" + totalQuestions);
     }
+
     $(gif).attr("width", "200");
     $(gif).attr("height", "200");
     $(".timer").hide();
     $(".forgif").append(gif);
 }
 
+// Game over, Report, Ask for new game
 function endGame() {
-    console.log("Game Over!!");
     $("#question").hide();
     $(".choice").hide();
     $(".timer").hide();
@@ -133,6 +137,7 @@ function endGame() {
 
 }
 
+// Reset
 function reset() {
 
     $("#reset").remove();
@@ -150,35 +155,36 @@ function reset() {
 }
 
 $(document).ready(function () {
+    // Click to start game
     $(".qBox").on('click', '#start', function () {
         $(".initHead").hide();
         $("#start").hide();
         fetchQuestions();
     });
 
+    // Click reset
     $(".qBox").on('click', '#reset', function () {
         reset();
     });
 
+    // Answer click
     $(".choice").on('click', '.btn', function () {
         if ($(this).text() == questions[questionTracker].c) {
-            console.log("correct!");
             evaluateAnswer("correct");
             if (totalQuestions - (questionTracker + 1)) {
                 clearInterval(timer);
                 setTimeout(nextQuestion, 3000);
             } else {
                 clearInterval(timer);
-                endGame();
+                setTimeout(endGame, 3000);
             }
         } else {
-            console.log("wrong:(");
             evaluateAnswer("wrong");
             if (totalQuestions - (questionTracker + 1)) {
                 clearInterval(timer);
                 setTimeout(nextQuestion, 3000);
             } else {
-                clearInterval(timer);
+                setTimeout(endGame, 3000);
                 endGame();
             }
         }
